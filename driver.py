d12ab9dd1ac5368a18064a1c528198dded1aeeed
JianshiZhao <jszhaopsu@gmail.com>
2016-07-04 19:01:38 -0400

    Changes to save memory and improve speed.
    In order to save memory and improve speed, the images are scaled down to
    160 by 120 pixels and truncated SVD is used to calculate the
    eigen-images.

--- driver.py
+++ driver.py
@@ -16,6 +16,9 @@ from sklearn.metrics import confusion_matrix
 from sklearn.decomposition import RandomizedPCA
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
+from sklearn.decomposition import TruncatedSVD
+import pdb
+import pyqtgraph as pg
 
 #
 driver_list = pandas.read_csv('./driver_imgs_list.csv')
@@ -25,7 +28,7 @@ driver_list = pandas.read_csv('./driver_imgs_list.csv')
 img_path = './imgs/train'
 img_class_folder = os.listdir(img_path)
 
-def get_img_list(img_path = './imgs/train'):
+def get_train_list(img_path = './imgs/train'):
     '''
     This function is to get all the img files in a list under the img_path 
     '''
@@ -38,11 +41,12 @@ def get_img_list(img_path = './imgs/train'):
     return img_list
     
 
-def load_imgs_to_array(img_list):
+def load_imgs_to_array(img_list, scale = 4):
     '''
     Load all the training images into a numpy array according to img_list. Each
     image is flatten to a one dimensional array. The last element of each row
-    is the label for that image. Image shape [640,480]
+    is the label for that image. Image shape [640,480]. For memory issues, images
+    are rescaled according to the desired scale.
     '''
     # Load and convert image to gray scale, and check the shape of the image. 
     img = numpy.array(Image.open(img_list.values()[0][0]).convert('L'))    
@@ -50,9 +54,10 @@ def load_imgs_to_array(img_list):
     img_label = {}
     for cls in img_list:
         print "Loading class :", cls
-        img_arr[cls] = numpy.empty((len(img_list[cls]),img.shape[0]*img.shape[1]),dtype='uint8')
+        
+        img_arr[cls] = numpy.empty((len(img_list[cls]),img.shape[0]/scale*img.shape[1]/scale),dtype='uint8')
         for i in range(len(img_list[cls])):
-            img_arr[cls][i] = numpy.array(Image.open(img_list[cls][i]).convert('L')).ravel()
+            img_arr[cls][i] = numpy.array(Image.open(img_list[cls][i]).resize((160,120),Image.ANTIALIAS).convert('L')).ravel()
         img_label[cls] = numpy.full((len(img_list[cls]),1),int(cls[1]),dtype = 'uint8')
         img_arr[cls] = numpy.hstack([img_arr[cls],img_label[cls]])
 
@@ -66,23 +71,43 @@ def load_and_save_img_arr():
     '''
     This function load the images to array and save to disk for further training
     '''
-    img_list = get_img_list()
+    img_list = get_train_list()
     flat_img_arr = load_imgs_to_array(img_list)
-    numpy.save('./flat_img_arr.npy',flat_img_arr)
+    numpy.save('./flat_img_arr_small.npy',flat_img_arr)
     print "flat_img_arr saved!"
     return flat_img_arr
+
+
+def get_test_list(img_path = './imgs/test'):
+    '''
+    This function is to get all the img files in a list under the test folder 
+    '''
+    test_imgs = os.listdir(img_path)
+    test_list = [os.path.join(img_path, img) for img in test_imgs if img.endswith('.jpg')]        
+    return test_list
+
+
+def load_test_img(test_path):
+    test_list = get_test_list(img_path = test_path)
+    test_img = numpy.empty((len(test_list),160*120),dtype = 'uint8')
+    for i in range(len(test_list)):
+        test_img[i] = numpy.array(Image.open(test_list[i]).resize((120,160),Image.ANTIALIAS).convert('L')).ravel()
+    return test_img    
+        
+
 
 
 #------------------------------ PCA and Training -----------------------------
 
 if __name__ == '__main__':
     try:
-        flat_img_arr = numpy.load('./flat_img_arr.npy')
+        flat_img_arr = numpy.load('./flat_img_arr_small.npy')
     except: 
         flat_img_arr = load_and_save_img_arr()
     
+    
     # Features X and Labels y are seperated    
-    img_shape = [640,480]
+    img_shape = [160,120]
     X = flat_img_arr[:,:-1]
     y = flat_img_arr[:,-1]
     
@@ -94,36 +119,44 @@ if __name__ == '__main__':
     print "n_samples: %d" % num_samples
     print "n_features: %d" % num_features
     print "n_classes: %d" % num_classes
-    
-    numpy.random.seed(42)
+
     # Split into training and testing set
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
-    
-    num_comp = 200
-    
+
+    num_comp = 50
+        
     print "Extracting the top %d eigen_images from %d training images" % (num_comp, num_features)
     t0 = time()
-    pca = RandomizedPCA(n_components = num_comp, whiten=True).fit(X_train)
-    print "done in %0.3fs" % (time() - t0)
+    svd = TruncatedSVD(n_components=num_comp)
+    svd.fit(X_train)
+#    pca = RandomizedPCA(n_components = num_comp, whiten=True).fit(X_train)
+    print "done in %0.3fs" % (time() - t0)    
     
-    eigen_images = pca.components_.reshape((num_comp,img_shape[0], img_shape[1]))
+    eigen_images = svd.components_.reshape((num_comp,img_shape[0], img_shape[1]))
     
+        
+    pg.image(eigen_images)
+
     print "Projecting the input data on the eigen_images orthonormal basis"
     t0 = time()
-    X_train_pca = pca.transform(X_train)
-    X_test_pca = pca.transform(X_test)
+    X_train_pca = svd.transform(X_train)
+    X_test_pca = svd.transform(X_test)
     print "done in %0.3fs" % (time() - t0)
     
     print "Fitting the classifier to the training set"
     
     t0 = time()
     
+#    param_grid = {
+#                 'C': [5,10,50,100,500],
+#                  'gamma': [1e-8,5e-8,1e-7,5e-7],
+#                  }
     param_grid = {
-                 'C': [1e3, 5e3, 1e4, 5e4, 1e5],
-                  'gamma': [0.0001, 0.0005, 0.001, 0.005, 0.01, 0.1],
+                 'C': [50],
+                  'gamma': [5e-8],
                   }
-    
-    clf = GridSearchCV(SVC(kernel='rbf', class_weight='balanced'), param_grid)
+   
+    clf = GridSearchCV(SVC(kernel='rbf', class_weight='balanced',probability = True), param_grid)
     clf = clf.fit(X_train_pca, y_train)
     print "done in %0.3fs" % (time() - t0)
     print "Best estimator found by grid search:"
@@ -132,10 +165,20 @@ if __name__ == '__main__':
     print "Predict driver behavior classes: "
     t0 = time()
     y_pred = clf.predict(X_test_pca)
+    y_pred2 = clf.predict(X_train_pca)
     print "done in %0.3fs" % (time() - t0)
     
     accuracy = accuracy_score(y_test,y_pred)
     print "Prediction Accuracy:", accuracy
+
+
+# ------------------------------ Predict the test images --------------------------
+      
+#    img_test = numpy.array(Image.open('./imgs/test/img_102149.jpg').resize((120,160),Image.ANTIALIAS).convert('L')).ravel()
+#    img_pca = svd.transform(img_test.reshape(1,-1))
+#    pred = clf.predict(img_pca)
+#    print pred
+
 
 
 
